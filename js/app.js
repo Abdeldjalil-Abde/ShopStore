f45fe2d49dcdb0a72670fef8aa6cf3fe4bac3ffa
MOH Lamine <mmmbbbhhh17@gmail.com>
2021-12-18 22:44:57 +0100

    start working with the url var

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 let header = document.querySelector(".top-par");
 
 window.addEventListener("scroll", (e) => {
-    if (window.scrollY > 200) {
+    if (window.scrollY > 50) {
         header.classList.add("scrolled");
     } else {
         header.classList.remove("scrolled");
--- js/market-place.js
+++ js/market-place.js
@@ -2,8 +2,15 @@
 const filterMene = document.querySelector(".filter-mene");
 const filterMeneBtn = document.querySelector(".filter-mene .open");
 
+const filterPar = document.querySelector(".filter-par");
+const toolsPar = document.querySelector(".tools-par");
+const market = document.querySelector(".market");
+
 filterMene.addEventListener("click", (e) => {
     filterMene.classList.toggle("active");
+    filterPar.classList.toggle("mene-opened");
+    toolsPar.classList.toggle("mene-opened");
+    market.classList.toggle("mene-opened");
 });
 
 //  //
@@ -19,12 +26,14 @@ const filterItems = document.querySelectorAll(".filter-par .filters .types li");
 const filtersFrameworkStyle = window.getComputedStyle(filterFramework);
 const filterStyle = window.getComputedStyle(filter);
 
+let filtersAppearNum = 5;
+
 let filtersFrameworkWidth = filtersFrameworkStyle
     .getPropertyValue("width")
     .split("px")[0];
 
 filterItems.forEach((obj) => {
-    obj.style.width = filtersFrameworkWidth / 3 + "px";
+    obj.style.width = filtersFrameworkWidth / filtersAppearNum + "px";
 });
 
 let itemsPointer = 0;
@@ -32,15 +41,91 @@ let itemsPointer = 0;
 filterLeftBtn.addEventListener("click", (e) => {
     if (itemsPointer > 0) {
         itemsPointer--;
-        filter.style.left = -itemsPointer * (filtersFrameworkWidth / 3) + "px";
+        filter.style.left =
+            -itemsPointer * (filtersFrameworkWidth / filtersAppearNum) + "px";
     }
 });
 
 filterRightBtn.addEventListener("click", (e) => {
-    if (itemsPointer < filterItems.length - 3) {
+    if (itemsPointer < filterItems.length - filtersAppearNum) {
         itemsPointer++;
-        filter.style.left = -itemsPointer * (filtersFrameworkWidth / 3) + "px";
+        filter.style.left =
+            -itemsPointer * (filtersFrameworkWidth / filtersAppearNum) + "px";
     }
 });
 
+//  //
+
+// URL //
+function getUrlParameters() {
+    let urlStr = window.location.search.split("?")[1].split("&");
+
+    let urlPar = [];
+    urlStr.forEach((obj) => {
+        let name = obj.split("=")[0];
+        let value = obj.split("=")[1];
+        value = value.replaceAll("%22", "");
+        value = value.replaceAll("%27", "");
+        value = value.replaceAll("%20", " ");
+
+        urlPar.push({ name, value });
+    });
+
+    return urlPar;
+}
+
+const filterMeneContainer = document.querySelector(".filter-mene .container");
+
+function setFilterMene(type) {
+    let typesItems = {
+        clothes: [
+            "shirts",
+            "pants",
+            "shoes",
+            "coats",
+            "hats",
+            "dresses",
+            "skirts",
+            "other",
+        ],
+    };
+
+    let items = "";
+
+    typesItems[type].forEach((obj) => {
+        items +=
+            '<a href="" class="item"><img src="../img/' +
+            obj +
+            '-logo.png" alt="" class="log" /><p class="content">' +
+            obj +
+            "</p></a>";
+    });
+
+    filterMeneContainer.innerHTML = items;
+}
+
+const filterItemsLink = document.querySelectorAll(
+    ".filter-par .filters .types li a"
+);
+
+let urlPar = getUrlParameters();
+urlPar.forEach((obj) => {
+    switch (obj["name"]) {
+        case "product_type":
+            {
+                filterItemsLink.forEach((filter) => {
+                    if (obj["value"] == filter.innerText.toLowerCase()) {
+                        filter.classList.add("active");
+                    }
+                });
+
+                setFilterMene(obj["value"]);
+            }
+
+            break;
+
+        default:
+            break;
+    }
+});
 //  //
