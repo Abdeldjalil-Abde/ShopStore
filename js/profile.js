957f435f72ebf19a6629be03610a5312a4012f26
MOH Lamine <mmmbbbhhh17@gmail.com>
2022-01-01 21:05:26 +0100

    click to change profile img

--- js/profile.js
+++ js/profile.js
@@ -1,15 +1,32 @@
-const visitForm = document.querySelector("section.visit")
-const editForm = document.querySelector("section.edit")
+// action button //
+const visitForm = document.querySelector("section.visit");
+const editForm = document.querySelector("section.edit");
 
-const editBtn = document.querySelector("section.visit .action .edit")
-const contactBtn = document.querySelector("section.visit .action .contact")
+const editBtn = document.querySelector("section.visit .action .edit");
+const contactBtn = document.querySelector("section.visit .action .contact");
 
-if(urlPar["profile_action"]=="owner visit"){
+if (urlPar["profile_action"] == "owner visit") {
     editBtn.classList.add("show");
-}else if(urlPar["profile_action"]=="user visit"){
+} else if (urlPar["profile_action"] == "user visit") {
     contactBtn.classList.add("show");
-}else if(urlPar["profile_action"]=="edit"){
+} else if (urlPar["profile_action"] == "edit") {
     visitForm.classList.add("hide");
     editForm.classList.add("show");
 }
+//  //
 
+// profile img //
+const img_input = document.querySelector(".edit .avatar .img input");
+const profileImg = document.querySelector(".edit .avatar .img img");
+
+img_input.addEventListener("change", () => {
+    const reader = new FileReader();
+
+    reader.addEventListener("load", () => {
+        profileImg.setAttribute("src", reader.result);
+    });
+
+    reader.readAsDataURL(img_input.files[0]);
+});
+
+//  //
