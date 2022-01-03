305be573c80bd193ea17397a2a8f31165d2a08b0
MOH Lamine <mmmbbbhhh17@gmail.com>
2022-01-03 12:05:16 +0100

    add the images system in add product page

--- js/add-product.js
+++ js/add-product.js
@@ -1,9 +1,7 @@
 // show price box when bay is checked //
-const typeBay = document.querySelector(
-    ".action-type .type .input:nth-child(1) input"
-);
+const typeBay = document.querySelector(".action-type .type .input.bay input");
 const typeExchange = document.querySelector(
-    ".action-type .type .input:nth-child(2) input"
+    ".action-type .type .input.exchange input"
 );
 
 const priceBox = document.querySelector(".action-type .price");
@@ -191,14 +189,6 @@ function setFilterTypesEvent(list) {
         obj.addEventListener("click", () => {
             activeType(list, index);
 
-            // set inputs value //
-            // productTypeField.innerHTML = `<img src="../img/icon/${productTypeSelected}/${filterTypesContent[index].innerHTML}.png" alt="" /><span>${filterTypesContent[index].innerHTML}</span>`;
-
-            // filterTypeInput.setAttribute(
-            //     "value",
-            //     productTypesContent[index].innerHTML
-            // );
-
             setProductTypeInput(
                 (action = "filter type"),
                 filterTypesContent[index].innerHTML
@@ -286,5 +276,228 @@ qualityStars.forEach((obj, index) => {
         //  //
     });
 });
+//  //
 
+// main picture //
+const main_img_set = document.querySelector(".main-picture .img input.isSet");
+const main_img_delete_btn = document.querySelector(
+    ".main-picture .img .delete"
+);
+
+const main_img_input = document.querySelector(".main-picture .img input.file");
+const mainImg = document.querySelector(".main-picture .img img");
+
+main_img_input.addEventListener("change", () => {
+    // img steed //
+    main_img_set.setAttribute("value", "true");
+    //  //
+
+    // enable the delete button //
+    main_img_delete_btn.classList.add("set");
+    //  //
+
+    // load the img //
+    const reader = new FileReader();
+
+    reader.addEventListener("load", () => {
+        mainImg.setAttribute("src", reader.result);
+    });
+
+    reader.readAsDataURL(main_img_input.files[0]);
+    //  //
+});
+
+main_img_delete_btn.addEventListener("click", () => {
+    // set the default img //
+    mainImg.setAttribute("src", "../img/icon/image.png");
+    //  //
+
+    // unset the input //
+    main_img_set.setAttribute("value", "false");
+    //  //
+
+    // disable the delete button //
+    main_img_delete_btn.classList.remove("set");
+    //  //
+});
+//  //
+
+// other pictures //
+let pictures;
+
+const picturesCounter = document.querySelector(
+    ".other-pictures-input input.counter"
+);
+let picturesInputs = document.querySelectorAll(
+    ".other-pictures-input input.file"
+);
+let lastPicturesInput = document.querySelector(
+    ".other-pictures-input input.file:last-child"
+);
+
+const picturesInputsContainer = document.querySelector(".other-pictures-input");
+const picturesContainer = document.querySelector(".other-pictures .container");
+
+let picturesImg;
+let picturesDeleteBtn;
+
+let picturesNum = 0;
+
+function addPic() {
+    let addBtn = document.querySelector(".other-pictures .container .add");
+
+    picturesContainer.removeChild(addBtn);
+
+    const reader = new FileReader();
+
+    reader.addEventListener("load", () => {
+        picturesNum++;
+        picturesCounter.setAttribute("value", picturesNum);
+
+        // add new input //
+        picturesInputsContainer.innerHTML += `<input class="file" type="file" id="img${
+            picturesNum + 1
+        }" />`;
+        //  //
+
+        // add the pic //
+        picturesContainer.innerHTML += `<div class="pic">
+            <img src="${reader.result}" alt="" />
+
+            <label class="change" for="img${picturesNum}">Change</label>
+            <div class="delete">
+                <img src="../img/icon/delete.png" alt="" />
+            </div>
+        </div>`;
+        //  //
+
+        // add the add button //
+        picturesContainer.innerHTML += `<label class="add" for="img${
+            picturesNum + 1
+        }"><img src="../img/icon/add.png" alt="" /></label>`;
+        //  //
+
+        // reset the pictures inputs //
+        picturesInputs = document.querySelectorAll(
+            ".other-pictures-input input.file"
+        );
+
+        picturesInputs.forEach((obj, index) => {
+            obj.setAttribute("onchange", `changePictureImg(${index})`);
+        });
+        //  //
+
+        // reset the last input //
+        lastPicturesInput = document.querySelector(
+            ".other-pictures-input input.file:last-child"
+        );
+
+        lastPicturesInput.setAttribute("onchange", "addPic()");
+        //  //
+
+        // reset the pictures //
+        pictures = document.querySelectorAll(".other-pictures .container .pic");
+        //  //
+
+        // reset the images //
+        picturesImg = document.querySelectorAll(
+            ".other-pictures .container .pic > img"
+        );
+        //  //
+
+        // reset the delete buttons //
+        picturesDeleteBtn = document.querySelectorAll(
+            ".other-pictures .container .pic > .delete"
+        );
+
+        picturesDeleteBtn.forEach((obj, index) => {
+            obj.setAttribute("onclick", `deleteImg(${index})`);
+        });
+        //  //
+    });
+
+    reader.readAsDataURL(lastPicturesInput.files[0]);
+}
+lastPicturesInput.setAttribute("onchange", "addPic()");
+
+function changePictureImg(index) {
+    if (index < picturesInputs.length - 1) {
+        const reader = new FileReader();
+
+        reader.addEventListener("load", () => {
+            picturesImg[index].setAttribute("src", reader.result);
+        });
+
+        reader.readAsDataURL(picturesInputs[index].files[0]);
+    }
+}
+
+let deleteButtonEvent = [];
+
+function deleteImg(i) {
+    picturesNum--;
+    picturesCounter.setAttribute("value", picturesNum);
+
+    picturesContainer.removeChild(pictures[i]); // remove the picture //
+    picturesInputsContainer.removeChild(picturesInputs[i]); // remove the picture input //
+
+    // rest the pictures inputs //
+    picturesInputs = document.querySelectorAll(
+        ".other-pictures-input input.file"
+    );
+
+    picturesInputs.forEach((obj, index) => {
+        obj.setAttribute("id", `img${index + 1}`);
+        obj.setAttribute("onchange", `changePictureImg(${index})`);
+    });
+    //  //
+
+    // reset the last input //
+    lastPicturesInput = document.querySelector(
+        ".other-pictures-input input.file:last-child"
+    );
+
+    lastPicturesInput.setAttribute("onchange", "addPic()");
+    //  //
+
+    // rest the pictures inputs label //
+    document.querySelectorAll(".pic label.change").forEach((obj, index) => {
+        obj.setAttribute("id", `img${index + 1}`);
+    });
+    //  //
+
+    // rest the change buttons //
+    document
+        .querySelectorAll(".other-pictures .container .pic .change")
+        .forEach((obj, index) => {
+            obj.setAttribute("for", `img${index + 1}`);
+        });
+    //  //
+
+    // reset the pictures //
+    pictures = document.querySelectorAll(".other-pictures .container .pic");
+    //  //
+
+    // reset the images //
+    picturesImg = document.querySelectorAll(
+        ".other-pictures .container .pic > img"
+    );
+    //  //
+
+    // reset the delete buttons //
+    picturesDeleteBtn = document.querySelectorAll(
+        ".other-pictures .container .pic > .delete"
+    );
+
+    picturesDeleteBtn.forEach((obj, index) => {
+        obj.setAttribute("onclick", `deleteImg(${index})`);
+    });
+    //  //
+
+    // reset the add button //
+    document
+        .querySelector(".other-pictures .container .add")
+        .setAttribute("for", `img${picturesNum + 1}`);
+    //  //
+}
 //  //
--- js/profile.js
+++ js/profile.js
@@ -19,14 +19,13 @@ if (urlPar["profile_action"] == "owner visit") {
 const img_input = document.querySelector(".edit .avatar .img input");
 const profileImg = document.querySelector(".edit .avatar .img img");
 
-img_input.addEventListener("change", () => {
+main_img_input.addEventListener("change", () => {
     const reader = new FileReader();
 
     reader.addEventListener("load", () => {
-        profileImg.setAttribute("src", reader.result);
+        mainImg.setAttribute("src", reader.result);
     });
 
-    reader.readAsDataURL(img_input.files[0]);
+    reader.readAsDataURL(main_img_input.files[0]);
 });
-
 //  //
