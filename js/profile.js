272985534ce7b6fb47db199cafb546133a496bd4
MOH Lamine <mmmbbbhhh17@gmail.com>
2022-01-05 22:54:02 +0100

    split the profile page

--- js/profile.js
+++ js/profile.js
@@ -1,20 +1,3 @@
-// action button //
-const visitForm = document.querySelector("section.visit");
-const editForm = document.querySelector("section.edit");
-
-const editBtn = document.querySelector("section.visit .action .edit");
-const contactBtn = document.querySelector("section.visit .action .contact");
-
-if (urlPar["profile_action"] == "owner visit") {
-    editBtn.classList.add("show");
-} else if (urlPar["profile_action"] == "user visit") {
-    contactBtn.classList.add("show");
-} else if (urlPar["profile_action"] == "edit") {
-    visitForm.classList.add("hide");
-    editForm.classList.add("show");
-}
-//  //
-
 // profile img //
 const img_input = document.querySelector(".edit .avatar .img input");
 const profileImg = document.querySelector(".edit .avatar .img img");
